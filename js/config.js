af7a58b00de40b9564aa4282dacfdaf8b5cc4b89
Giri7772005040 <231801040@rajalakshmi.edu.in>
2025-07-14 15:48:31 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_n2fHyOLlQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7t1co9ppbck1b5tjvkpo308smq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
