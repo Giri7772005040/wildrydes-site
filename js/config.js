cd5064e956e524c3a7373722d87a5fc011691adb
Giri7772005040 <231801040@rajalakshmi.edu.in>
2025-07-14 18:05:30 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_K8XpngyCW', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3gg2af1qgdt2r087hhk910igqj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://psgqfbtxue.execute-api.ap-south-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
