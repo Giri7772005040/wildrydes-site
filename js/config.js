656ae8bbd7920f72b8c30f5e596a3546e557f03f
Giri7772005040 <231801040@rajalakshmi.edu.in>
2025-07-14 18:10:29 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_HPSmEe4pZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3aupf0g77eb1omjeh8cqg2gpbg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://psgqfbtxue.execute-api.ap-south-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
